9c62629c35dec84762d3f00bb0cf303ab9d4fa3e
czhang03 <zcsx.ok@outlook.com>
2024-02-06 18:16:41 -0500

    remove unused code and uncomment

--- lib/KAT_Set.ml
+++ lib/KAT_Set.ml
@@ -1,53 +1,3 @@
-(** a kleene datatype, represents regular expressions **)
-(* type 'a test=
-  | Zero       
-  | One
-  | Prim of 'a
-  | And of 'a test * 'a test
-  | Or of 'a test * 'a test
-  | Not of 'a test *)
-
-  (* type katI =
-  | Zero
-  | One
-  | Value of string
-  | PBool of string
-  | Union of katI *  katI
-  | Conc of  katI * katI
-  | Star of katI
-  | Not of katI
-
-type katSort =
-  |BExp
-  |NBExp     (*Not a boolean expression*)
-
-type kat= katI * katSort
-let zero:kat = Zero,BExp
-let union(e1:kat) (e2: kat):kat=
-    match (e1,e2) with
-    |(Zero,_),_-> e2
-    |_,(Zero,_)-> e1
-    | (k1,BExp),(k2,BExp) -> Union(k1,k2),BExp
-    |(k1,_),(k2,_)-> Union(k1,k2),NBExp
-
-
-let not(e1:kat):kat=
-  match e1 with
-  |(k1,BExp)-> Not k1,BExp 
-  |_-> raise (Invalid_argument "Invalid argument to not")
-  
-(**examples to type check /tests**)
-module StringSet = Set.Make(String)
-let rec pBoolOf(k:katI):StringSet.t=
-  match k with
-  |One -> StringSet.empty
-  |PBool b-> StringSet.singleton b
-  |Conc(a,b)-> StringSet.union (pBoolOf a) (pBoolOf b)
-  |Not(b) -> pBoolOf b
-  (*TODO: Just to surpress the warning for now, remove when finished*)
-  | _ -> failwith "Unimplemented"
- *)
-
 type kat =
   | Zero
   | One
@@ -178,7 +128,7 @@ let atOf(primitive_boolset:StringSet.t):SStringSet.t =
 
 
 (**similar question to pBool**)
-(* let rec epsilon (atom:StringSet.t) (exp: kat) : bool = 
+let rec epsilon (atom:StringSet.t) (exp: kat) : bool = 
 if (expIsBExp== false) then 
 match exp with
 | Zero -> false
@@ -188,11 +138,11 @@ match exp with
 | Conc(a,b) -> epsilon (a,false) && epsilon (b,false)
 | Star _ -> true
 | _ -> false
-else raise (Invalid_argument "epsilon only takes KA expressions") *)
+else raise (Invalid_argument "epsilon only takes KA expressions")
 
 (** empty word for KAT expressions**)
-(* let eps (sum: KATISet.t): bool =
-  KATISet.exists (fun exp -> epsilon exp) sum *)
+let eps (sum: KATISet.t): bool =
+  KATISet.exists (fun exp -> epsilon exp) sum 
 
 
 (* existence of atom in R, set of KAT**)
