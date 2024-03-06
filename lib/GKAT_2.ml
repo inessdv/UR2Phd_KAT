96f7f8c553f7d62bdab6c12d394f6ce30cfed1a5
Hang Ji <jihang30@bu.edu>
2024-03-05 19:38:18 -0500

    convert GKAT to KAT

--- lib/GKAT_2.ml
+++ lib/GKAT_2.ml
@@ -17,5 +17,29 @@ type gkat =
   | While of bExp*kat
 
 
+(* type gkat =
+  | Pact of string
+  | Seq of gkat*gkat
+  | If of bExp*gkat*gkat
+  | Test of bExp
+  | While of bExp*gkat *)       
+
+let rec from_BE_to_KAT (exp:bExp):kat=
+  match exp with
+  | Zero -> PBool "true"
+  | One -> PBool "false"
+  | PBool b -> PBool b
+  | Or (b1,b2) -> Union (from_BE_to_KAT b1,from_BE_to_KAT b2)
+  | And (b1,b2) -> Conc(from_BE_to_KAT b1,from_BE_to_KAT b2)
+  | Not b -> Not (from_BE_to_KAT b)
+
+
+let from_GKAT_to_KAT (exp:gkat):kat=
+  match exp with
+  | Pact p ->  PAct p
+  | Seq (e1,e2) -> Conc (e1,e2)
+  | If (b,e1,e2)-> Union(Conc (from_BE_to_KAT b, e1),Conc ((Not (from_BE_to_KAT b)),e2))
+  | Test b->  from_BE_to_KAT b
+  | While (b,e) -> Conc(Star(Conc (from_BE_to_KAT b, e)),Not (from_BE_to_KAT b)) (*    (be)*notb    *)
 
 
