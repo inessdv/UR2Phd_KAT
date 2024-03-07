846555da81deb32a1c4ed3e1631ae9e71215793f
inessdv <inessdv@bu.edu>
2024-03-07 09:58:40 -0600

    equiv for gkat

--- lib/GKAT_2.ml
+++ lib/GKAT_2.ml
@@ -17,14 +17,9 @@ type gkat =
   | While of bExp*kat
 
 
-(* type gkat =
-  | Pact of string
-  | Seq of gkat*gkat
-  | If of bExp*gkat*gkat
-  | Test of bExp
-  | While of bExp*gkat *)       
+   
 
-let rec from_BE_to_KAT (exp:bExp):kat=
+let rec from_BE_to_KAT (exp:bExp): kat =
   match exp with
   | Zero -> PBool "true"
   | One -> PBool "false"
@@ -34,12 +29,15 @@ let rec from_BE_to_KAT (exp:bExp):kat=
   | Not b -> Not (from_BE_to_KAT b)
 
 
-let from_GKAT_to_KAT (exp:gkat):kat=
+let from_GKAT_to_KAT (exp:gkat): kat=
   match exp with
   | Pact p ->  PAct p
   | Seq (e1,e2) -> Conc (e1,e2)
   | If (b,e1,e2)-> Union(Conc (from_BE_to_KAT b, e1),Conc ((Not (from_BE_to_KAT b)),e2))
-  | Test b->  from_BE_to_KAT b
-  | While (b,e) -> Conc(Star(Conc (from_BE_to_KAT b, e)),Not (from_BE_to_KAT b)) (*    (be)*notb    *)
+  | Test b ->  from_BE_to_KAT b
+  | While (b,e) -> Conc(Star(Conc (from_BE_to_KAT b, e)),Not (from_BE_to_KAT b)) (*(be)*notb*)
 
+let gKat_equiv (exp1: gkat) (exp2: gkat): bool = 
+  let e1 = from_GKAT_to_KAT exp1 and e2 = from_GKAT_to_KAT exp2
+  in equiv e1 e2
 
