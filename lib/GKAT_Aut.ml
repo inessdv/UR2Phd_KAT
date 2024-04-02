8bcb7bfa31d12945cd4a6e5a619172e79ff5c85d
Hang Ji <jihang30@bu.edu>
2024-04-01 21:30:14 -0400

    sequence of Thompson construction

--- lib/GKAT_Aut.ml
+++ lib/GKAT_Aut.ml
@@ -1,5 +1,6 @@
 open Common
 open GKAT_2
+open PointedCoprod
 
 module PActSet = Set.Make (String)
 
@@ -25,7 +26,6 @@ module PAutomaton = struct
     p_acts: PActSet.t;
     states: State.Set.t;
     trans: trans;
-    start: State.t;
     p_start: Atom.t -> res;
   }
 end
@@ -41,21 +41,36 @@ let rec satisfy (at: Atom.t)(iota: bExp): bool =
   |And (i,b) -> satisfy at i && satisfy at b
   |Not b -> not (satisfy at b)
 
-let thompson_construct (exp:gkat)(p_act: PActSet.t)(p_test: PBoolSet.t): PAutomaton.t =
+(* let seq_transit_helper (state:State.Set.t)(at:Atom.t): res{
+
+} *)
+
+let rec thompson_construct (exp:gkat)(p_act: PActSet.t)(p_test: PBoolSet.t): PAutomaton.t =
   match exp with 
   | Pact p -> {
     p_tests = p_test;
     p_acts = p_act;
-    states = State.Set.singleton ;
-    trans = fun _ _ -> Accept;
-    p_start = fun at -> To(State.elem, p);
+    states = State.Set.singleton State.elem ;
+    trans = (fun _ _ -> Accept);
+    p_start = (fun _ -> To(State.elem, p));
   }
-  | Seq -> {
-    p_tests = ;
-    p_acts = ;
-    states =  ;
-    trans = ;
-    p_start = ;
+  | Seq (ex1,ex2) -> 
+    let s1=thompson_construct ex1 p_act p_test in
+    let s2=thompson_construct ex2 p_act p_test in 
+    let coprod= State.coprod s1.states s2.states in 
+    let right_states =State.Set.map coprod.to_right s2.states in 
+    
+    {
+    p_tests = p_test;
+    p_acts = p_act;
+    states = State.Set.union s1.states right_states;
+    trans = (fun state atom -> match coprod.from_coprod state with
+    |Right s->s2.trans (coprod.to_right s) atom
+    |Left s -> match s1.trans s atom with 
+    | Accept -> s2.p_start p_test    (*Not important input*)
+    | r -> r
+    ) ;
+    p_start = s1.p_start;
   } 
   | If -> {
     p_tests = ;
