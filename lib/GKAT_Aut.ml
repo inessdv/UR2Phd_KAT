5947e743625aed7ecffe72042e65ac985f1a2c07
inessdv <inessdv@bu.edu>
2024-04-03 12:54:49 -0400

    new res to left and res to right function

--- lib/GKAT_Aut.ml
+++ lib/GKAT_Aut.ml
@@ -26,6 +26,18 @@ module PAutomaton = struct
   }
 end
 
+let res_to_left (r: res) ( coprod: MakePosInt.coprodRes): res=
+  match r with
+  | Accept -> Accept
+  | Reject -> Reject
+  | To (s, p_act) -> To (coprod.to_left s, p_act)
+
+let res_to_right (r: res) ( coprod: MakePosInt.coprodRes): res =
+  match r with
+  | Accept -> Accept
+  | Reject -> Reject
+  | To (s, p_act) -> To (coprod.to_right s, p_act)
+
 let rec satisfy (at : Atom.t) (iota : bExp) : bool =
   match iota with
   | Zero -> false
@@ -50,24 +62,10 @@ let rec thompson_construct (exp : gkat) (p_act : PActSet.t)
         trans = (fun _ _ -> Accept);
         p_start = (fun _ -> To (State.elem, p));
       }
-  | Seq (ex1, ex2) ->
-      let auto1 = thompson_construct ex1 p_act p_test in
-      let auto2 = thompson_construct ex2 p_act p_test in
+  | Seq (exp1, exp2) ->
+      let auto1 = thompson_construct exp1 p_act p_test in
+      let auto2 = thompson_construct exp2 p_act p_test in
       let coprod, all_states = State.coprod_with_dom auto1.states auto2.states in
-      let res_to_left r =
-        match r with
-        | Accept -> Accept
-        | Reject -> Reject
-        | To (s, p_act) -> To (coprod.to_left s, p_act)
-      in
-
-      let res_to_right r =
-        match r with
-        | Accept -> Accept
-        | Reject -> Reject
-        | To (s, p_act) -> To (coprod.to_right s, p_act)
-      in
-      (* let right_states =State.Set.map coprod.to_right s2.states in  *)
       {
         p_tests = p_test;
         p_acts = p_act;
@@ -75,12 +73,11 @@ let rec thompson_construct (exp : gkat) (p_act : PActSet.t)
         trans =
           (fun state atom ->
             match coprod.from_coprod state with
-            | Right s -> auto2.trans s atom |> res_to_right
+            | Right s -> res_to_right (auto2.trans s atom) coprod
             | Left s -> (
                 match auto1.trans s atom with
-                | Accept ->
-                    auto2.p_start p_test |> res_to_right (*Not important input*)
-                | r -> r |> res_to_left));
+                | Accept -> res_to_right (auto2.p_start p_test) coprod
+                | r -> res_to_left r coprod));
         p_start =
           (fun at ->
             match auto1.p_start at with Accept -> auto2.p_start at | r -> r);
@@ -88,29 +85,17 @@ let rec thompson_construct (exp : gkat) (p_act : PActSet.t)
   | If (bexp, exp1, exp2) ->
       let auto1 = thompson_construct exp1 p_act p_test in
       let auto2 = thompson_construct exp2 p_act p_test in
-      let temp = Test bexp in
       let coprod, all_states = State.coprod_with_dom auto1.states auto2.states in
-      let res_to_left r =
-        match r with
-        | Accept -> Accept
-        | Reject -> Reject
-        | To (s, p_act) -> To (coprod.to_left s, p_act)
-      in
-
-      let res_to_right r =
-        match r with
-        | Accept -> Accept
-        | Reject -> Reject
-        | To (s, p_act) -> To (coprod.to_right s, p_act) in
       {
         p_tests = p_test;
         p_acts = p_act;
         states = all_states;
         trans = (fun state atom ->
           match coprod.from_coprod state with
-          | Right s -> auto2.trans s atom |> res_to_right
-          | Left s -> auto1.trans s atom |> res_to_left);
-        p_start = (fun atom -> if satisfy atom bexp then auto1.p_start atom |> res_to_left else auto2.p_start atom |> res_to_right);
+          | Right s -> res_to_right (auto2.trans s atom) coprod
+          | Left s -> res_to_left (auto1.trans s atom) coprod);
+        p_start = (fun atom -> if satisfy atom bexp then res_to_left (auto1.p_start atom) coprod
+         else res_to_right (auto2.p_start atom) coprod);
       }
 
   | Test b ->
