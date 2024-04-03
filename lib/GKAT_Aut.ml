46f498b01f8a3a55325b9d92b8f604a03a3f9d9a
inessdv <inessdv@bu.edu>
2024-04-03 13:48:48 -0400

    While constructor

--- lib/GKAT_Aut.ml
+++ lib/GKAT_Aut.ml
@@ -110,8 +110,11 @@ let rec thompson_construct (exp : gkat) (p_act : PActSet.t)
       }
   | While(bexp,exp) -> 
     let auto1 = thompson_construct exp p_act p_test in
-    { p_tests = p_test; p_acts = p_act; states = auto1.states; 
-    trans = (fun state atom -> match auto1.trans state atom with 
-    | Accept -> p_start atom
-    | r -> r); 
-    p_start = (fun atom -> if satisfy atom bexp then auto1.p_start atom else Accept);}
+    { p_tests = p_test; 
+      p_acts = p_act; 
+      states = auto1.states; 
+      trans = (fun state atom -> match auto1.trans state atom with 
+        | Accept -> Reject
+        | _ -> auto1.p_start atom); 
+      p_start = (fun atom -> if satisfy atom bexp then auto1.p_start atom else Accept);}
+
