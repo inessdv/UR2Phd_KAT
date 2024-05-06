34cc2d2dfc9d173a85052b3aa7f2d17437fa32ea
inessdv <inessdv@bu.edu>
2024-05-05 23:20:16 -0400

    printer GKAT start

--- lib/GKAT_2.ml
+++ lib/GKAT_2.ml
@@ -1,4 +1,5 @@
 open KAT_Set
+open KA_equiv.KAT_Set.Print
 
 type bExp =
   | Zero
@@ -43,3 +44,81 @@ let gKat_equiv (exp1 : gkat) (exp2 : gkat) : bool =
   let e1 = from_GKAT_to_KAT exp1 in
   let e2 = from_GKAT_to_KAT exp2 in
   equiv e1 e2
+
+module Print1 = struct
+  let gpprint (exp : gkat): string =
+    let kat_exp = from_GKAT_to_KAT exp in
+      pprint kat_exp
+
+end
+
+module Print2 = struct
+  let pprint (exp : gkat) =
+    (*helper method, takes a expression, output the string,
+          and **the precedence of the outer most expression** *)
+    let rec helper (exp : gkat) : string * int =
+      match exp with
+      | Pact p -> (p, 0)
+      | Seq (exp1,exp2) -> 
+        let str1, precedence1 = helper exp1 in
+          let str2, precedence2 = helper exp2 in
+          let str1' = if precedence1 <= 2 then str1 else "(" ^ str1 ^ ")" in
+          let str2' = if precedence2 < 2 then str2 else "(" ^ str2 ^ ")" in
+          (str1' ^ "*" ^ str2', 2)
+      | If (bexp, exp1, exp2) -> 
+        let str1, precedence1 = helper exp1 in
+          let str2, precedence2 = helper exp2 in
+          let str3, precedence3 = 
+            match bexp with
+            | Zero -> ("0", 0)
+            | One -> ("1", 0)
+            | PBool b -> (b, 0)
+            | Or (bexp1, bexp2) -> 
+              let b1, precedence1 = helper (from_BE_to_KAT bexp1) in
+              let b2, precedence2 = helper (from_BE_to_KAT bexp2) in
+              let b1' = if precedence1 <= 2 then str1 else "(" ^ b1 ^ ")" in
+              let b2' = if precedence2 < 2 then str2 else "(" ^ b2 ^ ")" in
+              (b1' ^ " or " ^ b2', 3)
+            | And (bexp1, bexp2)->
+              let b1, precedence1 = helper (from_BE_to_KAT bexp1) in
+              let b2, precedence2 = helper (from_BE_to_KAT bexp2) in
+              let b1' = if precedence1 <= 2 then str1 else "(" ^ b1 ^ ")" in
+              let b2' = if precedence2 < 2 then str2 else "(" ^ b2 ^ ")" in
+              (b1' ^ " and " ^ b2', 3)
+            | Not bexp1 -> 
+              let str, precedence = helper (from_BE_to_KAT bexp1) in
+              if precedence <= 1 then ("~" ^ str, 1) else ("~(" ^ str ^ ")", 1)
+        in
+          let str1' = if precedence1 <= 3 then str1 else "(" ^ str1 ^ ")" in
+          let str2' = if precedence2 < 3 then str2 else "(" ^ str2 ^ ")" in
+          (str1' ^ " + " ^str3^ str2', 3)
+      | Test bexp -> 
+        let str, precedence3 = 
+            match bexp with
+            | Zero -> ("0", 0)
+            | One -> ("1", 0)
+            | PBool b -> (b, 0)
+            | Or (bexp1, bexp2) -> 
+              let b1, precedence1 = helper (from_BE_to_KAT bexp1) in
+              let b2, precedence2 = helper (from_BE_to_KAT bexp2) in
+              let b1' = if precedence1 <= 2 then b1 else "(" ^ b1 ^ ")" in
+              let b2' = if precedence2 < 2 then b2 else "(" ^ b2 ^ ")" in
+              (b1' ^ " or " ^ b2', 3)
+            | And (bexp1, bexp2)->
+              let b1, precedence1 = helper (from_BE_to_KAT bexp1) in
+              let b2, precedence2 = helper (from_BE_to_KAT bexp2) in
+              let b1' = if precedence1 <= 2 then b1 else "(" ^ b1 ^ ")" in
+              let b2' = if precedence2 < 2 then b2 else "(" ^ b2 ^ ")" in
+              (b1' ^ " and " ^ b2', 3)
+            | Not bexp1 -> 
+              let str, precedence = helper (from_BE_to_KAT bexp1) in
+              if precedence <= 1 then ("~" ^ str, 1) else ("~(" ^ str ^ ")", 1) in
+              ( str, 1)
+      | While (bexp, exp) -> _
+  
+    let pprint_sum (expset : KATSet.t) : string =
+      let exp_list = KATSet.to_list expset in
+      (*I can do pipeline here*)
+      let string_list = List.map pprint exp_list in
+      String.concat " " string_list
+  end
