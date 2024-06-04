7eb08a1e3359deed893eb5b933dd001db941ef75
Hang Ji <jihang30@bu.edu>
2024-06-04 16:17:50 +0800

    changing all derivative'shelper function to list type

--- lib/GKAT_Symb.ml
+++ lib/GKAT_Symb.ml
@@ -166,67 +166,41 @@ module Derivatives = struct
   This uses the map representation of the derivative for the ease of implementation,
   and primitive action is encoded as a string *)
 
-  let combine_BE_with_a (be : BExp.t) (m : (BExp.t_, Exp.t * string) Hmap.t) :
-      (BExp.t_, Exp.t * string) Hmap.t =
-    let to_list = Hmap.bindings m in
-    let mapped_list = List.map (fun (a, b) -> (BExp.b_and a be, b)) to_list in
-    List.fold_left
-      (fun acc (a, (b, c)) -> Hmap.add a (b, c) acc)
-      Hmap.empty mapped_list
+  let combine_BE_with_a (be : BExp.t) (m : (BExp.t * (Exp.t * string)) list) :
+  (BExp.t * (Exp.t * string)) list =
+   List.map (fun (a, b) -> (BExp.b_and a be, b)) m
 
   let while_helper (be : BExp.t) (exp : Exp.t)
-      (m : (BExp.t_, Exp.t * string) Hmap.t) : (BExp.t_, Exp.t * string) Hmap.t
+      (m : (BExp.t * (Exp.t * string)) list) : (BExp.t * (Exp.t * string)) list
       =
-    let to_list = Hmap.bindings m in
-    let mapped_list =
       List.map
         (fun (a, (e', p)) ->
-          (BExp.b_and a be, (Exp.seq e' (Exp.while_do be exp), p)))
-        to_list
-    in
-    List.fold_left
-      (fun acc (a, (b, c)) -> Hmap.add a (b, c) acc)
-      Hmap.empty mapped_list
+          (BExp.b_and a be, (Exp.seq e' (Exp.while_do be exp), p))) m
 
   let sequence_helper_without_epsilon (exp2 : Exp.t)
-      (m : (BExp.t_, Exp.t * string) Hmap.t) : (BExp.t_, Exp.t * string) Hmap.t
+      (m : (BExp.t * (Exp.t * string)) list) : (BExp.t * (Exp.t * string)) list
       =
-    let to_list = Hmap.bindings m in
-    let mapped_list =
-      List.map (fun (b, (e', p)) -> (b, (Exp.seq e' exp2, p))) to_list
-    in
-    List.fold_left
-      (fun acc (a, (b, c)) -> Hmap.add a (b, c) acc)
-      Hmap.empty mapped_list
+      List.map (fun (b, (e', p)) -> (b, (Exp.seq e' exp2, p))) m
 
   let sequence_helper_with_epsilon (eps : BExp.t)
-      (m : (BExp.t_, Exp.t * string) Hmap.t) : (BExp.t_, Exp.t * string) Hmap.t
+      (m : (BExp.t * (Exp.t * string)) list) : (BExp.t * (Exp.t * string)) list
       =
-    let to_list = Hmap.bindings m in
-    let mapped_list =
-      List.map (fun (b, pair) -> (BExp.b_and b eps, pair)) to_list
-    in
-    List.fold_left
-      (fun acc (a, (b, c)) -> Hmap.add a (b, c) acc)
-      Hmap.empty mapped_list
+      List.map (fun (b, pair) -> (BExp.b_and b eps, pair)) m
 
-  let rec derivative (exp : Exp.t) : (BExp.t_, Exp.t * string) Hmap.t =
+  let rec derivative (exp : Exp.t) : (BExp.t * (Exp.t * string)) list =
     match exp.node with
-    | Test _ -> Hmap.empty
-    | Pact p -> Hmap.singleton BExp.one (Exp.test BExp.one, p)
+    | Test _ -> []
+    | Pact p -> [BExp.one,( Exp.test(BExp.one) , p)]
     | If (be, exp1, exp2) ->
-        Hmap.union
-          (fun _ _ _ -> None)
-          (combine_BE_with_a be (derivative exp1))
-          (combine_BE_with_a be (derivative exp2))
+        (** get rid of repetitions**)
+        (combine_BE_with_a be (derivative exp1)) @
+        (combine_BE_with_a be (derivative exp2))
     | Seq (e, f) ->
         let eps_of_e = epsilon e in
         let derivative_of_exp1 = derivative e in
         let derivative_of_exp2 = derivative f in
-        Hmap.union
-          (fun _ _ _ -> None)
-          (sequence_helper_without_epsilon f derivative_of_exp1)
-          (sequence_helper_with_epsilon eps_of_e derivative_of_exp2)
+        (sequence_helper_without_epsilon f derivative_of_exp1) @
+        (sequence_helper_with_epsilon eps_of_e derivative_of_exp2)
     | While (be, e) ->
         let derive_e = derivative e in
         while_helper be e derive_e
@@ -256,7 +230,7 @@ module Derivatives = struct
     else
       let explored = HSet.add exp explored in
       if BExp.is_false @@ epsilon exp then
-        let deriv = Hmap.bindings (derivative exp) in
+        let deriv = derivative exp in
         let next_exps = List.map (fun (_, (exp, _)) -> exp) deriv in
         check_dead_many explored next_exps
       else None
@@ -293,7 +267,7 @@ let product (psi_e:(BExp.t_ Hashcons.hash_consed * (Exp.t * string)) list) (psi_
        
   (** p(e) fucntion ρ(e) = ¬ ϵ(e) ∧ ¬ (⋁_{ψ ↦ (e', p) ∈ δ(e)} ψ)**)
   let rec reject (exp : Exp.t) : BExp.t =
-    let exp_derivatives = Hmap.bindings (derivative exp) in
+    let exp_derivatives = derivative exp in
     let epsilon = epsilon exp in
     let transitions = List.fold_left (fun acc (be,(_,_)) -> BExp.b_or acc be ) BExp.zero exp_derivatives in
       BExp.b_and (BExp.b_not epsilon) (BExp.b_not transitions)
@@ -307,234 +281,42 @@ let product (psi_e:(BExp.t_ Hashcons.hash_consed * (Exp.t * string)) list) (psi_
         (change input of equiv to include r1 and r2)
         **)
   let rec equiv (exp1 : Exp.t) (exp2 : Exp.t) : bool =
-    let exp1_ele = exp_ele exp1 in
-    let exp2_ele = exp_ele exp2 in
-
-    (** Check if the expressions have already been marked as equiv **)
-    if UnionFind.eq exp1_ele exp2_ele then true else
-
-    (** if both are dead, then they are equivalent **)
-    if ExpHSet.mem exp1 dead_states && ExpHSet.mem exp2 dead_states then true else
-    
-    (**  assert ϵ(e) = ϵ(f) **)
-      if not (BExp.equiv (epsilon exp1) (epsilon exp2)) then false else
-        let reject_atoms_of_exp1 = reject exp1 in 
-        let reject_atoms_of_exp2 = reject exp2 in 
-        let f_derivatives = Hmap.bindings (derivative exp2) in
-        let e_derivatives = Hmap.bindings (derivative exp1) in
-
-        List.for_all (fun(be,(exp,_))-> (is_dead exp ) && (BExp.is_false (BExp.b_and(reject_atoms_of_exp1) (be))))  f_derivatives
-        &&
-        List.for_all (fun(be,(exp,_))-> (is_dead exp ) && (BExp.is_false (BExp.b_and(reject_atoms_of_exp2) (be))))  e_derivatives
-        &&
-        let cross_product = product e_derivatives f_derivatives in 
-        let rec check_disjoint (cross_product: 
-          ((BExp.t_ Hashcons.hash_consed * (Exp.t * string))
-          * (BExp.t_ Hashcons.hash_consed * (Exp.t * string)))
-          list): bool =
-        match cross_product with
-        | [] -> true
-        | ((be1,(next_exp1,p)),(be2,(next_exp2,q)))::xs -> BExp.is_false (BExp.b_and be1 be2) ||
-          if p = q then (ignore @@UnionFind.union exp1_ele exp2_ele;
-          if equiv next_exp1 next_exp2 == true then check_disjoint xs else false)
-        else 
-          (if (is_dead(next_exp1) && is_dead(next_exp2)==true) then check_disjoint xs else false) in
-         check_disjoint cross_product
-        
-end
-
-module Equiv = struct
-  (** This module is DEPRECATED! Please disregard  *****)
-  module LiveExps (Size : sig
-    val size : int
-    (** the size of all the expressions needing to check.
-        
-    This value is used to optmize hash table implementation*)
-  end) : sig
-    val is_live : Exp.t -> bool
-  end = struct
-    (** Modules for computing live expressions. 
-    This module performs a modified Tarjan's algorithm to find live states.
-    
-    This is a seperate module, since it contains internal states.*)
-
-    open Derivatives
-    open Size
-    module HSet = Hashcons.Hset
-
-    (** current index to assign to the next node*)
-    let cur_idx : int ref = ref 0
-
-    (** A stack that contains all explored expression that haven't been assigned to a SCC*)
-    let stack : Exp.t Stack.t = Stack.create ()
-
-    type expInfo = {
-      idx : int;
-      (* the element used for SCC, which is represented as an union-find object*)
-      elem : Exp.t UnionFind.elem;
-      (* the lowest reachable index from the current expression *)
-      mutable low_link : int;
-      (* whether the expression is on the stack.
-
-         This variable is to aviod the linear time check of whether a expression is in the stack.*)
-      mutable on_stack : bool;
-    }
-
-    (** find the representative expression of for the SCC of the input `e` 
-        
-    This function assumes that the SCC of the expression `e` 
-    has already been collected as a union-find object*)
-    let rep (info_of_e : expInfo) : Exp.t =
-      UnionFind.get @@ UnionFind.find info_of_e.elem
-
-    (** A mutable hash table of expressions *)
-    module ExpTbl = Hashtbl.Make (struct
-      type t = Exp.t
-
-      let hash (e : t) = e.hkey
-      let equal e1 e2 = e1 == e2
-    end)
-
-    (** a mutable hash set of expressions *)
-    module ExpHSet = struct
-      type t = unit ExpTbl.t
-
-      let create : int -> t = ExpTbl.create
-      let add (exp : Exp.t) (s : t) : unit = ExpTbl.add s exp ()
-      let remove (exp : Exp.t) (s : t) : unit = ExpTbl.remove s exp
-
-      let mem (exp : Exp.t) (s : t) : bool =
-        Option.is_some @@ ExpTbl.find_opt s exp
-    end
-
-    (** tables mapping each expression to its info
-        
-    TODO: use the expression size to compute the reachable expressions, instead of a fixed number.*)
-    let info_tbl : expInfo ExpTbl.t = ExpTbl.create Size.size
-
-    (* Whether a expression transitions to a live scc
-
-       Notice that if a expression is in the set, then exp is necessarily live,
-       but if it is not, exp is not necessarily dead.
-       This only keeps track of the expression on the stack,
-       expressions not on the stack but explored are already collected as scc,
-       hence should use `live_scc` instead.*)
-    let to_live_scc : ExpHSet.t = ExpHSet.create size
-
-    (* all the currently discovered live scc,
-       each scc is marked by its "representing expression" in the union-find object.
-       for a expression `e`, the representing expression for its scc
-       can be computed by `rep_{scc} e`.*)
-    let live_scc : ExpHSet.t = ExpHSet.create size
-
-    (** get the info of a expression, will fail if the expression hasn't been explored*)
-    let info_of (e : Exp.t) : expInfo = ExpTbl.find info_tbl e
-
-    (** return whether `e` has been explored *)
-    let explored (e : Exp.t) : bool = ExpTbl.mem info_tbl e
-
-    (** visit the predecessor of e
-    
-    this will set the info of e to the correct value 
-    and compute the `detected_liveness` of `e`.
-    **this function assumes `e` is in the `info_of` table.** *)
-    let rec visit_predecessor_of (e : Exp.t) (info_of_e : expInfo) : unit =
-      let e_to_live_scc : bool ref = ref false in
-      (* visit the predecessor `e'` of `e`.*)
-      let visit_single_predecessor (e' : Exp.t) : unit =
-        (* f hasn't been explored, then explore f *)
-        if not @@ explored e' then (
-          visit e';
-          info_of_e.low_link <- min info_of_e.low_link (info_of e').low_link)
-        else
-          let info_of_e' = info_of e' in
-          if info_of_e'.on_stack then
-            (* if f has been explored, and on stack, then it is in the scc of e
-               Thus we update the `low_link` of `e`*)
-            info_of_e.low_link <- min info_of_e.low_link info_of_e'.low_link
-          else
-            (* if f explored but not on stack.
-                This means that f is part of an explored scc.
-               We will update the `e_to_live_scc` value *)
-            e_to_live_scc :=
-              !e_to_live_scc || ExpHSet.mem (rep info_of_e') live_scc
-      in
-      Hashcons.Hmap.iter
-        (fun _bexp (e', _p) -> visit_single_predecessor e')
-        (derivative e)
-
-    and construct_scc_of e : unit =
-      (* pop until `e` is reached, and output all the elements in a set *)
-      let rec pop_until_e () : Exp.t_ HSet.t =
-        let f = Stack.pop stack in
-        let info_of_f = info_of f in
-        info_of_f.on_stack <- false;
-        if f == e then HSet.singleton f
-        else
-          let s = pop_until_e () in
-          HSet.add f s
-      in
-      let popped = pop_until_e () in
-      (* whether the scc is live *)
-      let is_live =
-        (* whether the scc has a transition to live *)
-        HSet.exists (fun e -> ExpHSet.mem e to_live_scc) popped
-        (* whether the scc has an accepting transition *)
-        || HSet.exists (fun e -> not @@ BExp.is_false @@ epsilon e) popped
-      in
-      let info_of_e = info_of e in
-      (* union all the popped expression into a *)
-      if is_live then (
-        (* for each element in popped,
-           add it to the scc using union,
-           and remove it from `to_live_scc` to save memory*)
-        HSet.iter
-          (fun f ->
-            ignore @@ UnionFind.union (info_of f).elem info_of_e.elem;
-            ExpHSet.remove f to_live_scc)
-          popped;
-        (* add the representative of `e` to `live_scc`*)
-        ExpHSet.add (rep info_of_e) live_scc)
-      else
-        (* remove all the popped value from `to_live_scc` to save memory*)
-        HSet.iter (fun f -> ExpHSet.remove f to_live_scc) popped
-
-    (** Construct union find object that classifies the scc from `e`
-
-    This function is based on Tarjan's SCC algorithm;
-    it will iterate through all the reachable expressions from `e`,
-    and should only be called when e has not been explored.
-    *)
-    and visit (e : Exp.t) : unit =
-      (* init*)
-      Stack.push e stack;
-      let init_e_info =
-        {
-          idx = !cur_idx;
-          elem = UnionFind.make e;
-          low_link = !cur_idx;
-          on_stack = true;
-        }
-      in
-      ExpTbl.add info_tbl e init_e_info;
-      cur_idx := !cur_idx + 1;
-
-      (* visit all the reachable expressions recursively *)
-      visit_predecessor_of e init_e_info;
-
-      (* the computed info of e*)
-      let info_of_e = info_of e in
-
-      (* collect scc if all the scc is explored *)
-      if info_of_e.low_link = info_of_e.idx then construct_scc_of e
-
-    let is_live (e : Exp.t) : bool =
-      match ExpTbl.find_opt info_tbl e with
-      (* `e` has not been explored, visit `e` first.*)
-      | None ->
-          visit e;
-          ExpHSet.mem (rep @@ info_of e) live_scc
-      (* `e` has been explored *)
-      | Some info_of_e -> ExpHSet.mem (rep info_of_e) live_scc
-  end
-end
+          let exp1_ele = exp_ele exp1 in
+          let exp2_ele = exp_ele exp2 in
+      
+          (** Check if the expressions have already been marked as equiv **)
+          if UnionFind.eq exp1_ele exp2_ele then true else
+      
+          (** if both are dead, then they are equivalent **)
+          if ExpHSet.mem exp1 dead_states then is_dead exp2 else
+          if ExpHSet.mem exp2 dead_states then is_dead exp1 else
+          
+          (**  assert ϵ(e) = ϵ(f)   assert and try catch  design custom exception **)
+            if not (BExp.equiv (epsilon exp1) (epsilon exp2)) then false else
+              let reject_atoms_of_exp1 = reject exp1 in 
+              let reject_atoms_of_exp2 = reject exp2 in 
+              let f_derivatives = derivative exp2 in
+              let e_derivatives = derivative exp1 in
+              (**ASSERT**)
+              List.for_all (fun(be,(exp,_))-> 
+                is_dead exp  || BExp.is_false (BExp.b_and reject_atoms_of_exp1 be))  f_derivatives
+              &&
+              List.for_all (fun(be,(exp,_))-> 
+                is_dead exp  || BExp.is_false (BExp.b_and reject_atoms_of_exp2 be))  e_derivatives
+              &&
+              
+              let cross_product = product e_derivatives f_derivatives in 
+              let rec check_disjoint (cross_product: 
+                ((BExp.t_ Hashcons.hash_consed * (Exp.t * string))
+                * (BExp.t_ Hashcons.hash_consed * (Exp.t * string)))
+                list): bool =
+              match cross_product with
+              | [] -> true
+              | ((be1,(next_exp1,p)),(be2,(next_exp2,q)))::xs -> BExp.is_false (BExp.b_and be1 be2) ||
+                if p == q then (ignore @@UnionFind.union exp1_ele exp2_ele;
+                if equiv next_exp1 next_exp2 then check_disjoint xs else false)
+              else 
+                (if (is_dead(next_exp1) && is_dead(next_exp2)) then check_disjoint xs else false) in
+               check_disjoint cross_product
+              
+      end
