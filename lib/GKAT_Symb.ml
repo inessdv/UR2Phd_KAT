e48565d5a47bcef7b8b637a289a17d8f5790c461
Hang Ji <jihang30@bu.edu>
2024-06-10 17:36:10 +0800

    minor change in name

--- lib/GKAT_Symb.ml
+++ lib/GKAT_Symb.ml
@@ -266,7 +266,7 @@ let product (psi_e:(BExp.t_ Hashcons.hash_consed * (Exp.t * string)) list) (psi_
 
 
 
-  let rec equiv (exp1 : Exp.t) (exp2 : Exp.t) (reject1: BExp.t) (reject2: BExp.t) : bool =
+  let rec equiv_helper (exp1 : Exp.t) (exp2 : Exp.t) (reject1: BExp.t) (reject2: BExp.t) : bool =
           let exp1_ele = exp_ele exp1 in
           let exp2_ele = exp_ele exp2 in
       
@@ -293,12 +293,12 @@ let product (psi_e:(BExp.t_ Hashcons.hash_consed * (Exp.t * string)) list) (psi_
               List.for_all(fun ((be1,(next_exp1,p)),(be2,(next_exp2,q)))->
                 BExp.is_false (BExp.b_and be1 be2) ||
                 if p = q then (ignore @@UnionFind.union exp1_ele exp2_ele;
-                if equiv next_exp1 next_exp2 reject1 reject2 then true else false)
+                if equiv_helper next_exp1 next_exp2 reject1 reject2 then true else false)
               else 
                 (if (is_dead(next_exp1) && is_dead(next_exp2)) then true else false)) (product e_derivatives f_derivatives)
-  let rec equiv_helper (exp1 : Exp.t) (exp2 : Exp.t) : bool =
+  let rec equiv (exp1 : Exp.t) (exp2 : Exp.t) : bool =
     let reject1 = reject exp1 in
     let reject2 = reject exp2 in
-      equiv exp1 exp2 reject1 reject2
+      equiv_helper exp1 exp2 reject1 reject2
 
     end
