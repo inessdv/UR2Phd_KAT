6b32180e8a1d89b5a877ec7aa4dda4be5568cdd5
czhang03 <czhang03_gh@tutanota.com>
2024-07-31 11:37:44 -0400

    uncomment all the tests

--- test/KA_equiv_test.ml
+++ test/KA_equiv_test.ml
@@ -4,14 +4,14 @@ let _ =
   run_test_tt_main
     ("all tests"
     >::: [
-           (*KA_test.epsilon_tests;
+           KA_test.epsilon_tests;
            KA_test.deriv_test;
            KAT_test.epsilon_tests;
            KAT_test.deriv_test;
            GKAT_test_qcheck.test_equiv_deriv; 
            GKAT_test_qcheck.test_equiv_aut;  
-           GKAT_test_qcheck.test_aut_vs_deriv
-           GKAT_test_qcheck.test_equiv_symb; *)
+           GKAT_test_qcheck.test_aut_vs_deriv;
+           GKAT_test_qcheck.test_equiv_symb;
            GKAT_test_qcheck.test_symb_vs_gkat; 
-           (*GKAT_test_qcheck.test_symb_vs_aut*)
+           GKAT_test_qcheck.test_symb_vs_aut
          ])
