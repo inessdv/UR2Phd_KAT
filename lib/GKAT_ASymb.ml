778314a14618fb75d43fbfe54d2054038fd498b6
Hang Ji <jihang30@bu.edu>
2024-10-21 15:17:50 -0400

    Start the Aut Symbolic

---
+++ lib/GKAT_ASymb.ml
@@ -0,0 +1,322 @@
+open Common
+module PActSet = Set.Make (String)
+module BExp = struct
+  (** Module for working with boolean expressions *)
+  type t_node = (t_ * Z3.Expr.expr) 
+  
+  and t = t_node Hashcons.hash_consed
+  (** The type for a hashconsed boolean expression *)
+
+  (* The internal type of the boolean expression*)
+  and t_ =
+    | Zero
+    | One
+    | PBool of string * int
+    | Or of t * t
+    | And of t * t
+    | Not of t
+
+  module T_node = struct
+    type t = t_node
+
+    let equal (t1, _) (t2, _) =
+      match (t1, t2) with
+      | Zero, Zero -> true
+      | One, One -> true
+      | PBool (_, i), PBool (_, j) -> i == j
+      | Or (x1, y1), Or (x2, y2) -> x1 == x2 && y1 == y2
+      | And (x1, y1), And (x2, y2) -> x1 == x2 && y1 == y2
+      | Not x1, Not x2 -> x1 == x2
+      | _ -> false
+
+    let hash (t, _) =
+      match t with
+      | Zero -> Hashtbl.hash `Zero
+      | One -> Hashtbl.hash `One
+      | PBool (_, i) -> Hashtbl.hash (`PBool i)
+      | Or (x, y) -> Hashtbl.hash (`Or (x.hkey, y.hkey))
+      | And (x, y) -> Hashtbl.hash (`And (x.hkey, y.hkey))
+      | Not x -> Hashtbl.hash (`Not x.hkey)
+  end
+
+  module HashT = Hashcons.Make (T_node)
+
+  (** table used for hash consing 
+    notice because of hash consing, we can build *)
+  let tbl = HashT.create 251
+
+  let z3_empty_ctx = Z3.mk_context []
+  let hashcons = HashT.hashcons tbl
+  let zero : t = hashcons @@ (Zero, Z3.Boolean.mk_false z3_empty_ctx)
+  let one : t = hashcons @@ (One, Z3.Boolean.mk_true z3_empty_ctx)
+
+  let pBool (str : string) : t =
+    hashcons
+    @@ (PBool (str, Hashtbl.hash str), Z3.Boolean.mk_const_s z3_empty_ctx str)
+
+  let b_not (b1 : t) : t =
+    if b1 == one then zero
+    else if b1 == zero then one
+    else
+      let _, b1_ = b1.node in
+      hashcons @@ (Not b1, Z3.Boolean.mk_not z3_empty_ctx b1_)
+
+  let b_or (b1 : t) (b2 : t) : t =
+    if b1 == one then one
+    else if b2 == one then one
+    else if b1 == zero then b2
+    else if b2 == zero then b1
+    else if b1 == b2 then b1
+    else if b1 == b_not b2 then one
+    else
+      let _, b1_ = b1.node in
+      let _, b2_ = b2.node in
+      hashcons @@ (Or (b1, b2), Z3.Boolean.mk_or z3_empty_ctx [ b1_; b2_ ])
+
+  let b_and (b1 : t) (b2 : t) : t =
+    if b1 == one then b2
+    else if b2 == one then b1
+    else if b1 == zero then zero
+    else if b2 == zero then zero
+    else if b1 == b2 then b1
+    else if b1 == b_not b2 then zero
+    else
+      let _, b1_ = b1.node in
+      let _, b2_ = b2.node in
+      hashcons @@ (And (b1, b2), Z3.Boolean.mk_and z3_empty_ctx [ b1_; b2_ ])
+
+  (** convert a boolean expression to z3 expression *)
+  let to_z3 (b : t) : Z3.Expr.expr = snd b.node
+
+  let solver = Z3.Solver.mk_solver z3_empty_ctx None
+
+  (** test if a boolean expression is constant false
+
+  In other word, whether it is unsatisfiable. *)
+  let is_false (b : t) : bool =
+    match Z3.Solver.check solver [ to_z3 b ] with
+    | Z3.Solver.UNSATISFIABLE -> true
+    | _ -> false
+
+  (** Test if two boolean expressions is semantically equivelant. *)
+  let equiv (b1 : t) (b2 : t) : bool =
+    let iff_exp = Z3.Boolean.mk_iff z3_empty_ctx (to_z3 b1) (to_z3 b2) in
+    let not_iff_exp = Z3.Boolean.mk_not z3_empty_ctx iff_exp in 
+    (* if ¬ (b1 ↔ b2) is unsatisfiable, then b1 ↔ b2 is a tautology,
+       thus b1 and b2 are semantically equivalent.*)
+    match Z3.Solver.check solver [ not_iff_exp ] with
+    | Z3.Solver.UNSATISFIABLE -> true
+    | _ -> false
+
+  let pprint e = Z3.Expr.to_string @@ to_z3 e
+end
+
+module Exp = struct
+  type t = t_ Hashcons.hash_consed
+  (** hashconsed GKAT expression*)
+
+  and t_ =
+    | Pact of string * int
+    | Seq of t * t
+    | If of BExp.t * t * t
+    | Test of BExp.t
+    | While of BExp.t * t
+
+  module T_node = struct
+    type t = t_
+
+    let equal t1 t2 =
+      match (t1, t2) with
+      | Pact (_, i), Pact (_, j) -> i == j
+      | Seq (x1, y1), Seq (x2, y2) -> x1 == x2 && y1 == y2
+      | If (b1, x1, y1), If (b2, x2, y2) -> b1 == b2 && x1 == x2 && y1 == y2
+      | Test b1, Test b2 -> b1 == b2
+      | While (b1, t1), While (b2, t2) -> b1 == b2 && t1 == t2
+      | _ -> false
+
+    let hash t =
+      match t with
+      | Pact (_, i) -> Hashtbl.hash (`Pact i)
+      | Seq (x, y) -> Hashtbl.hash (`Seq (x.hkey, y.hkey))
+      | If (b, x, y) -> Hashtbl.hash (`If (b.hkey, x.hkey, y.hkey))
+      | Test b -> Hashtbl.hash (`Text b.hkey)
+      | While (b, x) -> Hashtbl.hash (`While (b.hkey, x.hkey))
+  end
+
+  module HashT = Hashcons.Make (T_node)
+
+  (** table used for hash consing 
+  notice because of hash consing, we can build *)
+  let tbl = HashT.create 251
+
+  let hashcons : t_ -> t = HashT.hashcons tbl
+  let p_act (p : string) : t = hashcons @@ Pact (p, Hashtbl.hash p)
+  let test (b : BExp.t) : t = hashcons @@ Test b
+  let skip : t = test BExp.one
+  let fail : t = test BExp.zero
+
+  let seq (e : t) (f : t) : t =
+    match (e.node, f.node) with
+    | Test a, Test b -> test (BExp.b_and a b)
+    | _ ->
+        if e == skip then f
+        else if f == skip then e
+        else if e == fail then fail
+        else if f == fail then fail
+        else hashcons @@ Seq (e, f)
+
+  let if_then_else (b : BExp.t) (e : t) (f : t) : t =
+    if b == BExp.one then e
+    else if b == BExp.zero then f
+    else if e == fail then seq (test @@ BExp.b_not b) f
+    else if f == fail then seq (test b) e
+    else hashcons @@ If (b, e, f)
+
+  let while_do (b : BExp.t) (e : t) : t = hashcons @@ While (b, e)
+    (* if b == BExp.zero then skip
+    else if b == BExp.one then fail
+    else if e == skip || e == fail then test @@ BExp.b_not b 
+    else  *)
+
+    (** Return the number of primitive actions in the expression*)
+    let rec num_pact (e : t) =
+      match e.node with
+      | Pact _ -> 1
+      | Seq (e1, e2) -> num_pact e1 + num_pact e2
+      | If (_, e1, e2) -> num_pact e1 + num_pact e2
+      | Test _ -> 0
+      | While (_, e1) -> num_pact e1
+  
+    (** Return the number of test expression in the expression*)
+    let rec num_bexp (e : t) =
+      match e.node with
+      | Pact _ -> 0
+      | Seq (e1, e2) -> num_bexp e1 + num_bexp e2
+      | If (_, e1, e2) -> 1 + num_bexp e1 + num_bexp e2
+      | Test _ -> 1
+      | While (_, e1) -> 1 + num_bexp e1
+  
+    (** number of sequencing operation in the input expression *)
+    let rec num_seq (e : t) =
+      match e.node with
+      | Pact _ -> 0
+      | Seq (e1, e2) -> 1 + num_seq e1 + num_seq e2
+      | If (_, e1, e2) -> num_seq e1 + num_seq e2
+      | Test _ -> 0
+      | While (_, e1) -> num_seq e1
+  
+    (** number of if statements in the input expression *)
+    let rec num_if (e : t) =
+      match e.node with
+      | Pact _ -> 0
+      | Seq (e1, e2) -> num_if e1 + num_if e2
+      | If (_, e1, e2) -> 1 + num_if e1 + num_if e2
+      | Test _ -> 0
+      | While (_, e1) -> num_if e1
+  
+    (** number of while loop in the input expression *)
+    let rec num_while (e : t) =
+      match e.node with
+      | Pact _ -> 0
+      | Seq (e1, e2) -> num_while e1 + num_while e2
+      | If (_, e1, e2) -> num_while e1 + num_while e2
+      | Test _ -> 0
+      | While (_, e1) -> 1 + num_while e1
+  
+    let pprint (exp : t) =
+      let rec helper (exp : t) : string * int =
+        match exp.node with
+        | Pact (p, _) -> (p, 0)
+        | Seq (e1, e2) ->
+            let s1, p1 = helper e1 in
+            let s2, p2 = helper e2 in
+            let s1' = if p1 < 2 then s1 else "(" ^ s1 ^ ")" in
+            let s2' = if p2 < 2 then s2 else "(" ^ s2 ^ ")" in
+            (s1' ^ " ; " ^ s2', 2)
+        | If (b, e1, e2) ->
+            let bs = BExp.pprint b in
+            let s1, p1 = helper e1 in
+            let s2, p2 = helper e2 in
+            let s1' = if p1 <= 3 then s1 else "(" ^ s1 ^ ")" in
+            let s2' = if p2 < 3 then s2 else "(" ^ s2 ^ ")" in
+            ("if " ^ bs ^ " then " ^ s1' ^ " else " ^ s2', 3)
+        | Test b ->
+            let bs = BExp.pprint b in
+            (bs, 1)
+        | While (b, e) ->
+            let bs = BExp.pprint b in
+            let s, p = helper e in
+            let s' = if p <= 1 then s else "(" ^ s ^ ")" in
+            ("while " ^ bs ^ " do " ^ s' ^ " done", 1)
+      in
+      fst @@ helper exp
+end
+
+
+module Derivatives = struct
+  type res = Accept | Reject | To of State.t * int(*changed from Pact*)
+  type trans = State.t -> BExp.t -> res
+  type be_input=State.t -> BExp.t
+  type automaton = {
+    p_tests : BExp.t;
+    accept_BE_input : be_input;
+    states : State.Set.t;
+    trans : trans;
+    start : State.t;
+  }
+  type pAutomaton = {
+    p_accept : BExp.t; (* ϵ* : The overall boolean expression accept *)
+    accept : State.t -> BExp.t; (*  ϵ̂ *)(*all the atoms that the input state accepts*)
+    states : State.Set.t;
+    trans : trans; (* δ̂ all the atoms that will transition to a certain result*)
+    p_trans : BExp.t -> res; 
+    (* δ* set of (BExp.t , (s,p))  when compare, compare the tag of BE let compare b1 b2 = compare b1.tag b2.tag*)
+  }
+  type epsilonTrans = State.t -> res
+
+
+  (* let rec satisfy (at : BExp.t) (iota : BExp.t) : bool =
+    match iota.node with
+    | Zero -> false
+    | BExp.One -> true
+    | PBool b -> Atom.mem b at
+    | Or (i, b) -> satisfy at i || satisfy at b
+    | And (i, b) -> satisfy at i && satisfy at b
+    | Not b -> not (satisfy at b)
+   *)
+  let rec thompson_construct (exp : Exp.t) : pAutomaton =
+  match exp.node with
+  | Test b->
+      {
+        p_tests = b; (* ϵ* : The overall boolean expression *)
+        accept_BE_input = (fun _ -> Accept); (*  ϵ̂ *)
+        states = State.Set.empty;(* S *)
+        trans = (fun _  _ -> Accept);  (* δ̂ all the atoms that will transition to a certain result*)
+        p_start = (fun _ -> Reject); (* δ*  ???*)
+      }
+
+  |Pact (_, p) ->  
+    {        
+      p_tests = BExp.zero; (* ϵ* : The overall boolean expression *)
+      accept_BE_input = (fun _ -> Accept); (*  ϵ̂ *)
+      states = State.Set.singleton State.elem;(* S *)
+      trans = (fun _  _ -> Reject);  (* why it is empty? *)
+      p_start = (fun _ -> To (State.elem, p)); (* δ*  ???*)
+    }
+  |If(b,exp1,exp2) ->
+    let auto1 = thompson_construct exp1  in
+    let auto2 = thompson_construct exp2  in
+    let coprod, all_states =
+        State.coprod_with_dom auto1.states auto2.states in 
+        
+    {
+    states= all_states;
+    p_tests= BExp.b_or (BExp.b_and (b) (auto1.p_tests))(BExp.b_and (BExp.b_not (b)) (auto2.p_tests));
+    accept_BE_input=_;
+    trans = _;
+    p_start= _ ;
+    }
+  | _->
+
+
+end
